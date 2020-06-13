13d9685407922b7b2b985f588025026f745c8c42
Scarlett Baez <s.baez1010@gmail.com>
2020-06-13 19:48:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,3 +9,24 @@ function forLoop(array) {
 
 	return array;
 }
+
+function whileLoop(n) {
+	while (n > 0) {
+		console.log(--n);
+	}
+
+	return 'done';
+}
+
+function doWhileLoop(num) {
+	var i = 0;
+
+	function incrementVariable() {
+		i = i + 1;
+    return i;
+	}
+
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+}
